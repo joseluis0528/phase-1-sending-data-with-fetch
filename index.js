39d9c500952c1f840dfff7e863082cd5fe67f657
Jose <joseluis0528@gmail.com>
2023-04-09 14:19:47 -0500

    working on POST fetch request

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            user: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+    })
+}
