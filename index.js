7fcf25ca30a478f74ba142b27a5b1c033ffb7a5e
Jose <joseluis0528@gmail.com>
2023-04-10 21:49:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 // Add your code here
 
 function submitData(userName, userEmail) {
-    fetch('http://localhost:3000/users', {
+    
+    const configUsers = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
         body: JSON.stringify({
-            user: userName,
+            name: userName,
             email: userEmail,
         })
-    })
+    }
+    
+    return fetch('http://localhost:3000/users', configUsers)
     .then(response => response.json())
     .then(data => {
-        
+        document.body.append(data.id);
+    })
+    .catch(error => {
+        alert('Not valid!');
+        document.body.append(error.message);
     })
 }
